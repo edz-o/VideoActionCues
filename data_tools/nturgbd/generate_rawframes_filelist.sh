aaf2ab2144681135075ed9280a82c20363e86c2e
edz-o <edwardz.amg@gmail.com>
2019-11-19 15:04:23 -0500

    fix bug

--- data_tools/nturgbd/generate_rawframes_filelist.sh
+++ data_tools/nturgbd/generate_rawframes_filelist.sh
@@ -4,6 +4,6 @@ cd ../../
 #PYTHONPATH=. python data_tools/build_file_list.py nturgbd data/nturgbd/rawframes_train/ --level 1 --format rawframes --num_split 2 --subset train --shuffle
 #echo "Train filelist for rawframes generated."
 
-PYTHONPATH=. python data_tools/build_file_list.py nturgbd data/nturgbd/rawframes/ --level 1 --format rawframes --num_split 3 --subset val --shuffle
+PYTHONPATH=. python data_tools/build_file_list.py nturgbd data/nturgbd/rawframes_val/ --level 1 --format rawframes --num_split 3 --subset val --shuffle
 echo "Val filelist for rawframes generated."
 cd data_tools/nturgbd/
